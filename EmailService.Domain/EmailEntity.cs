14da90183bb9856d998d6110aaece67d1041aa0c
Gustavo Cuerva <86535275+GustavoCuerva@users.noreply.github.com>
2025-09-14 19:16:16 -0300

    Create EmailEntity

---
+++ EmailService.Domain/EmailEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace EmailService.Domain;
+
+public class EmailEntity
+{
+	public string To { get; private set; } = string.Empty;
+	public string Subject { get; private set; } = string.Empty;
+	public string Body { get; private set; } = string.Empty;
+
+	public static EmailEntity Create(string to, string subject, string body)
+	{
+		var email = new EmailEntity()
+		{
+			To = to,
+			Subject = subject,
+			Body = body
+		};
+
+		return email;
+	}
+}
